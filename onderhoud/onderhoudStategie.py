64cd77146b256e8f46866a346c5bd2f3f4de4929
Jan-jaha <tdrewes@student.che.nl>
2026-05-28 12:03:00 +0200

    onderhoudstategie

---
+++ onderhoud/onderhoudStategie.py
@@ -0,0 +1,27 @@
+import uuid
+from typing import List
+
+class OnderhoudStrategie:
+    def __init__(self, id: uuid.UUID, kunstwerk_id: uuid.UUID, frequentie: int, onderhoudeisen: List[str]):
+        self.__id = id
+        self.__kunstwerk_id = kunstwerk_id
+        self.__frequentie = frequentie
+        self.__onderhoudeisen = onderhoudeisen
+
+    @classmethod
+    def json_verwerken(cls, data: dict) -> "OnderhoudStrategie":
+        return cls(
+            id=uuid.UUID(data["id"]),
+            kunstwerk_id=uuid.UUID(data["kunstwerk_id"]),
+            frequentie=data["frequentie"],
+            onderhoudeisen=data["onderhoudeisen"]
+        )
+
+    def get_id(self) -> uuid.UUID:
+        return self.__id
+
+    def get_kunstwerk_id(self) -> uuid.UUID:
+        return self.__kunstwerk_id
+
+    def get_onderhoudeisen(self) -> List[str]:
+        return self.__onderhoudeisen
